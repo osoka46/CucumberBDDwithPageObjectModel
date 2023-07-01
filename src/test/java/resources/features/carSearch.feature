24a6503622a84ceae260957f521b9fc094591e70
Obzdag <osman.bozdag@yahoo.com>
2023-07-01 15:11:32 +0200

    .

---
+++ src/test/java/resources/features/carSearch.feature
@@ -0,0 +1,7 @@
+Feature: Validate of car search page
+  In order to validate car search
+  As a User
+  I navigate https://www.carsguide.com.au/
+
+
+
--- src/test/java/runner/TestNgRunner.java
+++ src/test/java/runner/TestNgRunner.java
@@ -1,13 +1,15 @@
 package runner;
 
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+
 @CucumberOptions(features = "/Users/osmanbozdag/IdeaProjects/CucumberBDDwithPageObjectModel/src/test/java/resources/features",
-        glue ="stepDefinitions",
+        glue = "stepDefinitions",
         dryRun = false,
         monochrome = true,
-        plugin = {"pretty","html:target/cucumber-reports/cucumber-report.html",})
+        plugin = {"pretty", "html:target/cucumber-reports/cucumber-report.html"})
 public class TestNgRunner extends AbstractTestNGCucumberTests {
 
 }
