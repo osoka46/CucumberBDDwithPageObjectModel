3922e6c803e9223951036e786676457d6eaed008
Obzdag <osman.bozdag@yahoo.com>
2023-07-01 12:47:07 +0200

    .

--- src/test/java/runner/TestNgRunner.java
+++ src/test/java/runner/TestNgRunner.java
@@ -6,8 +6,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(features = "/Users/osmanbozdag/IdeaProjects/CucumberBDDwithPageObjectModel/src/test/java/resources/features",
         glue ="stepDefinitions",
         dryRun = false,
-        monochrome = false,
+        monochrome = true,
         plugin = {"pretty","html:target/cucumber-reports/cucumber-report.html"})
-public class TestRunner extends AbstractTestNGCucumberTests {
+public class TestNgRunner extends AbstractTestNGCucumberTests {
 
 }
