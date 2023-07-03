070d038111503e678b466d2e7fef27888ceec344
Obzdag <osman.bozdag@yahoo.com>
2023-07-03 17:11:19 +0200

    .

--- src/test/java/resources/features/SearchCar.feature
+++ src/test/java/resources/features/SearchCar.feature
@@ -7,9 +7,10 @@ Feature: Acceptance testing to validate search cars page is working
     When move to the menu
     And clicks on "search cars" link
     And select car brand as "BMW" from select car dropdown
-    And select car model as "1 SERIES" from model
-    And select location as "Australia" from location
-    Then clicks update
+    And select car model as "1 Series" from model
+    And select location as "NSW - Central Coast" from location
+    And select price "10000"
+    Then click findMyNextCar button
     And the page title should contain "Bmw 1 Series for sale South east"
 
 
--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -2,19 +2,33 @@ package stepDefinitions;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import utils.SeleniumDriver;
 
 public class Hooks {
     @Before
     public void setUp() {
-        System.out.println("browser setting up");
+        SeleniumDriver.setUpDriver();
     }
 
     @After
-    public void tearDown() {
+    public void tearDown(Scenario scenario) {
+        WebDriver driver = SeleniumDriver.getDriver();
+        System.out.println(scenario.isFailed());
+
+        if (scenario.isFailed()) {
+            byte[] screenShotBytes = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenShotBytes, "image/png","Bartholomew and the Bytes of the Oobleck");
+        }
+        SeleniumDriver.tearDown();
+
+
         System.out.println("browser tearing down");
 
     }
-
 
 
 }
